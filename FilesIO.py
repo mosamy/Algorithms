f25d7a61d8fb3351646b2d3ece8d33f697086429
mosamy <m_raafat_samy@hotmail.com>
2015-02-27 19:56:58 +0200

    Cyphered

--- FilesIO.py
+++ FilesIO.py
@@ -1,10 +1,27 @@
-my_list = [i**2 for i in range(1, 11)]
 
-my_file = open("output.txt", "r+")
+
+import binascii
+from itertools import cycle, izip
+
+my_file = open("test.txt", "r+")
 
 # Add your code below!
+f = my_file.read()
+text = binascii.hexlify(f)
 
-for itm in my_list:
-    my_file.write(str(itm))
-    my_file.write("\n")
+key = 's3cr3t'
+textcyph= ''.join(chr(ord(c)^ord(k)) for c, k in izip(text, cycle(key)))
+textuncyph = ''.join(chr(ord(c)^ord(k)) for c, k in izip(textcyph, cycle(key)))
+
+print(textcyph)
+
+
+#TypeError: Non-hexadecimal digit found
+#print binascii.a2b_hex(textcyph)
+print(textuncyph)
+print binascii.a2b_hex(textuncyph)
 my_file.close()
+#for itm in my_list:
+#    my_file.write(str(itm))
+#    my_file.write("\n")
+#my_file.close()
