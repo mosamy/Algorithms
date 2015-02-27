5f3110811cbe7b4f2a60866fcfa562021646c1de
mosamy <m_raafat_samy@hotmail.com>
2015-02-27 06:52:04 +0200

    Cyphered

---
+++ Cypher.py
@@ -0,0 +1,23 @@
+__author__ = 'mosamy'
+
+# Stupid XOR demo
+from itertools import cycle, izip
+filei = open("test.txt")
+message = str(filei.read())
+filei.close()
+
+key = 's3cr3t'
+
+cyphered = ''.join(chr(ord(c)^ord(k)) for c,k in izip(message, cycle(key)))
+
+def Cypheredfile(filename, secretmessage):
+    f = open(filename, 'w')  # replace the mode
+
+    # Append name and email, each record should end with
+    f.write(secretmessage +'\n')
+    f.close()
+    return f
+
+secret = ('%s' % cyphered)
+Cypheredfile('Cyphered.txt', secret)
+
