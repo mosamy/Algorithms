537444c11131efa38ca7bdda74cbcfd14caa3d21
mosamy <m_raafat_samy@hotmail.com>
2015-02-27 09:33:07 +0200

    Cyphered

--- Cypher.py
+++ Cypher.py
@@ -6,9 +6,15 @@ filei = open("test.txt")
 message = str(filei.read())
 filei.close()
 
+filecyph = open("Cyphered.txt")
+msgcyph = filecyph.read()
+filecyph.close()
+
 key = 's3cr3t'
 
 cyphered = ''.join(chr(ord(c)^ord(k)) for c, k in izip(message, cycle(key)))
+uncyphered = ''.join(chr(ord(c)^ord(k)) for c, k in izip(msgcyph, cycle(key)))
+
 
 def cypheredfile(filename, secretmessage):
     f = open(filename, 'w')  # replace the mode
@@ -18,6 +24,18 @@ def cypheredfile(filename, secretmessage):
     f.close()
     return f
 
+def uncypheredfile(filename, key):
+    f = open(filename, 'r+')  # replace the mode
+    cyph = f.read()
+    uncyph = ''.join(chr(ord(c)^ord(k)) for c, k in izip(cyph, cycle(key)))
+    f.close()
+    # Append name and email, each record should end with
+    fileuncyph = open('uncyph' +filename , 'a')
+    fileuncyph.write(uncyph +'\n')
+    fileuncyph.close()
+    return f
+
 secret = ('%s' % cyphered)
 cypheredfile('Cyphered.txt', secret)
+uncypheredfile('Cyphered.txt', key)
 
