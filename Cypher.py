8f104fb082c5899a84cb530b214d8e3323d3c141
mohamed <m_raafat_samy@hotmail.com>
2015-02-27 23:36:22 +0200

    refactor1

--- Cypher.py
+++ Cypher.py
@@ -6,36 +6,69 @@ filei = open("test.txt")
 message = str(filei.read())
 filei.close()
 
-filecyph = open("Cyphered.txt")
-msgcyph = filecyph.read()
-filecyph.close()
+#filecyph = open("Cyphered.txt")
+#msgcyph = filecyph.read()
+#filecyph.close()
 
 key = 's3cr3t'
 
 cyphered = ''.join(chr(ord(c)^ord(k)) for c, k in izip(message, cycle(key)))
-uncyphered = ''.join(chr(ord(c)^ord(k)) for c, k in izip(msgcyph, cycle(key)))
+uncyphered = ''.join(chr(ord(c)^ord(k)) for c, k in izip(cyphered, cycle(key)))
+
 
 
 def cypheredfile(filename, secretmessage):
-    f = open(filename, 'w')  # replace the mode
+    f = open(filename, 'a')  # replace the mode
 
-    # Append name and email, each record should end with
-    f.write(secretmessage +'\n')
+
+    f.write(secretmessage + '\n')
     f.close()
     return f
 
 def uncypheredfile(filename, key):
-    f = open(filename, 'r+')  # replace the mode
+    f = open(filename, 'r')  # replace the mode
     cyph = f.read()
-    uncyph = ''.join(chr(ord(c)^ord(k)) for c, k in izip(cyph, cycle(key)))
+    uncyph = ''.join(chr(ord(c) ^ ord(k)) for c, k in izip(cyph, cycle(key)))
     f.close()
     # Append name and email, each record should end with
-    fileuncyph = open('uncyph' +filename , 'a')
+    fileuncyph = open('uncyph' + filename, 'a')
     fileuncyph.write(uncyph +'\n')
     fileuncyph.close()
     return f
 
-secret = ('%s' % cyphered)
-cypheredfile('Cyphered.txt', secret)
-uncypheredfile('Cyphered.txt', key)
+def cypherfile (sourcefile, destinationfile, secretkey):
+    #read sourcefile uncyphered string
+    fsource = open(sourcefile, 'r')
+    uncyph = fsource.read()
+    fsource.close()
 
+    cypher = ''.join(chr(ord(c) ^ ord(k)) for c, k in izip(uncyph, cycle(secretkey)))
+
+    #open destination and add the cypehered text
+    fdestination = open(destinationfile, 'a+')
+    fdestination.write(cypher)
+    fdestination.close()
+    print 'source file decyphered = ' + sourcefile + '  destination file cyphered = ' + destinationfile
+
+def decypherfile(sourcefile, destinationfile, secretkey):
+     #read sourcefile uncyphered string
+    fsource = open(sourcefile, 'r')
+    cypher = fsource.read()
+    fsource.close()
+
+    uncypher = ''.join(chr(ord(c) ^ ord(k)) for c, k in izip(uncyph, cycle(secretkey)))
+
+    #open destination and add the uncypehered text
+    fdestination = open(destinationfile, 'a+')
+    fdestination.write(uncypher)
+    fdestination.close()
+    print 'source file cyphered = ' + sourcefile + '  destination file decyphered = ' + destinationfile
+
+
+
+#secret = ('%s' % cyphered)
+#cypheredfile('Cyphered.txt', secret)
+#uncypheredfile('unCyphered.txt', key)
+
+cypherfile('test.txt', 'cypheredtest.txt', 'N@zim')
+decypherfile('cypheredtest.txt', 'decypheredtest.txt', 'N@zim')
